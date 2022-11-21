a5e42cc1103dcf39ba4a84e197840d643ca18a8e
DarioRossi99 <rssdra99@gmail.com>
2022-11-21 15:00:14 +0100

    inserimento img più testo

---
+++ js/vue.js
@@ -0,0 +1,10 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            titolo: "belin sto proprio male in sto periodo",
+            img:"img/galata-morente.webp",
+        }
+    }
+}).mount("#app")
